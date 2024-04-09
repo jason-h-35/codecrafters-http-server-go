2b4f2bee2df4643918d04639f927f3215604da5a
Jason Hatton <jason@hatton.io>
2024-04-09 14:15:53 -0500

    comments

--- app/server.go
+++ app/server.go
@@ -7,17 +7,20 @@ import (
 )
 
 func main() {
+	// listen
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	// conn from listener
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 	defer conn.Close()
+	// send response over conn
 	response := []byte("HTTP/1.1 200 OK\r\n\r\n")
 	_, err = conn.Write(response)
 	if err != nil {
