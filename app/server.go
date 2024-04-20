c284d2c576717d8197d00f5ffb153fca794c353c
Jason Hatton <jason@hatton.io>
2024-04-20 12:58:05 -0500

    use struct instead of string functions inline

--- app/server.go
+++ app/server.go
@@ -16,7 +16,6 @@ type HTTPRequest struct {
 	host      string
 	userAgent string
 	accept    string
-	bytes     []byte
 }
 
 func NewHTTPRequest(buf []byte) HTTPRequest {
@@ -33,27 +32,23 @@ func NewHTTPRequest(buf []byte) HTTPRequest {
 		host:      tokens[1][1],
 		userAgent: tokens[2][1],
 		accept:    tokens[3][1],
-		bytes:     buf,
 	}
 }
 
 func handleRequest(requestBuf []byte) []byte {
-	request := string(requestBuf)
-	fmt.Printf(request)
-	lines := strings.Split(request, "\r\n")
-	startLine := strings.Split(lines[0], " ")
-	var response []byte
 
-	if strings.Compare(startLine[1], "/") == 0 {
+	request := NewHTTPRequest(requestBuf)
+	fmt.Printf("%s", requestBuf)
+	var response []byte
+	if strings.Compare(request.resource, "/") == 0 {
 		response = []byte("HTTP/1.1 200 OK\r\n\r\n")
-	} else if strings.HasPrefix(startLine[1], "/echo/") {
-		body := []byte(startLine[1][6:])
+	} else if strings.HasPrefix(request.resource, "/echo/") {
+		body := []byte(request.resource[6:])
 		headerString := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %v\r\n\r\n", len(body))
 		header := []byte(headerString)
 		response = append(header, body...)
-	} else if strings.Compare(startLine[1], "/user-agent") == 0 {
-		userAgent := strings.Split(lines[2], " ")[1]
-		body := []byte(userAgent)
+	} else if strings.Compare(request.resource, "/user-agent") == 0 {
+		body := []byte(request.userAgent)
 		headerString := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %v\r\n\r\n", len(body))
 		header := []byte(headerString)
 		response = append(header, body...)
